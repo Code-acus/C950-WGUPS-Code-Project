027755754b83711f2dcfa519ce2d2ec0001e6da4
Harrison <47501747+Code-acus@users.noreply.github.com>
2022-06-23 12:58:03 -0700

    Update hashmap.py
    Defined constructor for HashTable

--- hashmap.py
+++ hashmap.py
@@ -1,1 +1,15 @@
 # Hash map class
+
+class HashTable:
+    def __init__(self):
+        self.size = 11
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+    def hashfunction(self, key, size):
+        return key % size
+
+    def rehash(self, oldhash, size):
+        return (oldhash + 1) % size
+
+    def put(self, key, data):
