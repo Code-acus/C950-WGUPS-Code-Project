78a0a48707f8c4a19d7fa0bf16169be829d938c0
Harrison <47501747+Code-acus@users.noreply.github.com>
2022-06-25 17:38:10 -0700

    Added code to hashtable for new features and fucntionality

---
+++ class HashTableCreate.py
@@ -0,0 +1,4 @@
+class HashTableCreate:
+    def __init__(self, key, package):
+        self.key = key
+        self.package = package
--- hashmap.py
+++ hashmap.py
@@ -1,15 +1,31 @@
 # Hash map class
 
 class HashTable:
-    def __init__(self):
-        self.size = 11
-        self.slots = [None] * self.size
-        self.data = [None] * self.size
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.table = []
+        for value in range(capacity):
+            self.table.append([])
 
-    def hashfunction(self, key, size):
-        return key % size
+    # Create a hash key which should run in O(1)
+    def new_hash_key(self, key):
+        return int(key) % len(self.capacity)
 
-    def rehash(self, oldhash, size):
-        return (oldhash + 1) % size
+    # Insert package into hash table which should run in O(n)
+    def package_insert(self, key, value):
+        key_hash = self.create_key_hash(key)
+        key_value = [key, value]
 
-    def put(self, key, data):
+        if self.map[key_hash] is None:
+            self.map[key_hash] = list([key_value])
+            return True
+        else:
+            for pair in self.map[key_hash]:
+                if pair[0] == key:
+                    pair[1] = key_value
+                    return True
+            self.map[key_hash].append(key_value)
+            return True
+
+    # Get package from hash table which should run in O(n)
+
